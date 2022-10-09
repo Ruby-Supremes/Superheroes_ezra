9408fa4e60052de846ecf85fe7380571aec33e7a
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-10-09 13:03:25 +0300

    add validation to hero_power model

--- app/models/hero_power.rb
+++ app/models/hero_power.rb
@@ -1,4 +1,5 @@
 class HeroPower < ApplicationRecord
+    validates :strength, inclusion: { in: %w(Strong Weak Average), message: "must be one of %{value}" }
     belongs_to :hero
     belongs_to :power
 end
