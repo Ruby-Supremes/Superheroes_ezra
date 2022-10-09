01b0164945c19a254e7d44f77bfef9cd33e13896
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-10-09 15:51:06 +0300

    get heroes data from the db

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,2 +1,17 @@
 class HeroesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :hero_not_found
+    def index
+        heroes = Hero.all
+        render json: heroes
+    end
+
+    def show
+        hero = Hero.find(params[:id])
+        render json: hero
+    end
+    private
+    def hero_not_found
+        render json: { error: "Hero not found" }, status: 404
+    end
 end
+
