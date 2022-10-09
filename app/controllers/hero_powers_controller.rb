0a697090135d7bf05ead2dad983ef8b8523be530
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-10-09 16:21:09 +0300

    create a heropower using route

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,2 +1,19 @@
 class HeroPowersController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :status_error
+       #POST
+       def create
+        hero_power = HeroPower.create!(hero_power_params)
+        hero= Hero.find(params[:hero_id])
+        render json: hero_power
+       end
+
+    private 
+
+    def hero_power_params
+        params.permit(:strength, :power_id, :hero_id)
+    end
+
+    def status_error
+        render json: { error:  ["validation errors"]}
+      end
 end
